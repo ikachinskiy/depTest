64331a391202bfe20b43421afa052935badadf10
Kachinskiy Ivan <ivan@kachinskiy.ru>
2017-12-21 02:07:03 +0300

    Первичная настройка (1-й этап) - твиг и веб-панель

---
+++ public/info.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: smartnet
+ * Date: 21.12.17
+ * Time: 1:22
+ */
+phpinfo();
---
+++ src/Controller/MainController.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: smartnet
+ * Date: 21.12.17
+ * Time: 1:48
+ */
+
+namespace App\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class MainController extends Controller
+{
+
+    /**
+     * @Route("/", name="index")
+     */
+    public function index() {
+
+        return $this->render('main/index.html.twig');
+    }
+
+}
