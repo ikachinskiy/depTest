77d5ba43f37f515ee9bd344a975b6debd9699ff2
smartnet <ivan@kachinskiy.ru>
2017-12-21 17:40:26 +0300

    Начальная стадия проекта T4 - стартовый шаблон приложения на фреймворке Symfony 4

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -22,12 +22,4 @@ class MainController extends Controller
         return $this->render('main/index.html.twig');
     }
 
-    /**
-     * @Route("/new", name="new")
-     */
-    public function newPage() {
-
-        return $this->render('main/new.html.twig');
-    }
-
 }
